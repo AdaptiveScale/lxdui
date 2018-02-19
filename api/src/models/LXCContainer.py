60c1ed56e7d61698322c22621d48a6b5d6075a1f
ajdin <ajdin.idrizi@gmail.com>
2018-02-19 14:05:09 +0100

    added: Create container with basic requirements added: Validation library for input validation added: Generic response util

--- api/src/models/LXCContainer.py
+++ api/src/models/LXCContainer.py
@@ -1,3 +1,5 @@
+from pylxd.exceptions import LXDAPIException
+
 from api.src.models.LXDModule import LXDModule
 
 class LXCContainer(LXDModule):
@@ -6,21 +8,34 @@ class LXCContainer(LXDModule):
         self.remoteHost = '127.0.0.1'
         if not self.input.get('name'):
             raise ValueError('Missing container name.')
-        if 'remoteHost' in self.input:
-            self.remoteHost = self.input.get('remoteHost')
 
         super(LXCContainer, self).__init__(remoteHost=self.remoteHost)
 
     def info(self):
-        return self.client.api.containers[self.input.get('name')].get().json()
+        return self.client.api.containers[self.input.get('name')].get().json()['metadata']
 
     def create(self):
-        pass
+        data = {
+            'name':self.input.get('name'),
+            'source':{
+                'type':'image',
+                'alias':self.input.get('image')
+            }
+        }
+        try:
+            self.client.containers.create(data, wait=True)
+            self.start()
+            return self.info()
+        except LXDAPIException as e:
+            raise ValueError(e.response.json()['metadata']['err'] or e.response.json()['error'])
 
     def delete(self):
         container = self.client.containers.get(self.input.get('name'))
         #container.stop()
-        container.delete()
+        try:
+            container.delete()
+        except LXDAPIException as e:
+            raise ValueError(e.response.json()['error'])
 
     def update(self):
         pass
