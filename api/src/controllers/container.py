4254ba087bcba94722ad9b36a2ef1e49fb517465
nuhi <nuhi@adaptivescale.com>
2018-02-21 14:25:17 +0100

    LXDAPI Exception Handling

--- api/src/controllers/container.py
+++ api/src/controllers/container.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
+from flask import jsonify
+
 from api.src.helpers.container_schema import doValidate
 
 from api.src.models.LXCContainer import LXCContainer
@@ -18,33 +20,23 @@ def getContainer(name):
     client = LXCContainer({'name':name})
     return response.reply(client.info())
 
-
 @container_api.route('/', methods=['POST'])
-def createContainer():
-    input = request.get_json(silent=True)
-    validation = doValidate(input)
-    if validation:
-        return response.reply(message=validation.message, status=403)
-
-    input = [input] if not isinstance(input, list) else input
-
-    try:
-        result = []
-        for container in input:
-            client = LXCContainer(container)
-            result.append(client.create())
-        return response.reply(result)
-    except ValueError as ex:
-        return response.reply(message=ex.__str__(), status=403)
+def create_container():
+        input = request.get_json()
+        validation = doValidate(input)
+        if validation:
+                return jsonify({'validationError':validation.message})
+        try:
+                client = LXCContainer(input)
+                return jsonify(client.create())
+        except ValueError as ex:
+            return response.reply(ex.__str__(), 'Failed', 403)
 
 @container_api.route('/<string:name>', methods=['DELETE'])
 def deleteContainer(name):
-    input = request.get_json(silent=True)
-    force = False if input == None else input.get('force')
-    print(force)
     try:
         container = LXCContainer({'name': name})
-        container.delete(force)
+        container.delete()
         return response.reply(None)
     except ValueError as ex:
-        return response.reply(message=ex.__str__(), status=403)
+        return response.reply(ex.__str__(), 'Failed', 403)
