9877771eb304dee3a29b5ec88a3c7085c4ab5bc0
erenisr <erenisramadani@gmail.com>
2018-02-23 10:35:02 +0100

    Updated: Profile controller and Profile model to handle validation and error handling according to the reviews on the previous PR

--- api/src/models/LXCProfile.py
+++ api/src/models/LXCProfile.py
@@ -1,7 +1,6 @@
 from api.src.models.LXDModule import LXDModule
 
 from pylxd import Client
-from pylxd.exceptions import LXDAPIException
 
 
 class LXCProfile(LXDModule):
@@ -9,12 +8,6 @@ class LXCProfile(LXDModule):
     def __init__(self, input):
         self.client = Client()
         self.input = input
-        if not self.input.get('name'):
-            raise ValueError('Missing profile name.')
-        # if not self.input.get('config'):
-        #    raise ValueError('Missing config.')
-        # if not self.input.get('devices'):
-        #    raise ValueError('Missing devices.')
 
     def info(self):
         try:
@@ -26,19 +19,19 @@ class LXCProfile(LXDModule):
         try:
             self.client.profiles.create(self.input.get('name'), config=self.input.get('config'),
                                         devices=self.input.get('devices'))
+            return self.info()
         except Exception as e:
             raise ValueError(e)
-        return self.info()
 
     def deleteProfile(self):
         try:
             return self.client.api.profiles[self.input.get('name')].delete(json=self.input).json()
-        except LXDAPIException as e:
-            raise ValueError(e.response.json()['error'])
+        except Exception as e:
+            raise ValueError(e)
 
     def updateProfile(self):
         try:
             self.client.api.profiles[self.input.get('name')].put(json=self.input).json()['metadata']
+            return self.info()
         except Exception as e:
             raise ValueError(e)
-        return self.info()
