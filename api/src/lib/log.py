92ebbb00de96d581440409e505b0d67eeca3f72c
vetoni <thajdari@cask.co>
2018-02-25 21:21:56 -0600

    Refactored CLI implementation

---
+++ api/src/lib/log.py
@@ -0,0 +1,34 @@
+import logging.handlers
+
+
+class Log:
+    def __init__(self, __name__):
+        self.file = 'lxdui.log'
+        self.configure(__name__)
+
+    def configure(self, __name__):
+        logging.basicConfig(level=logging.DEBUG,
+                        format='[%(asctime)s] %(name)-12s %(levelname)-8s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M.%S',
+                        filename=self.file,
+                        filemode='w')
+
+        # write INFO messages or higher to the sys.stderr
+        sh = logging.StreamHandler()
+        slh = logging.handlers.SysLogHandler(address='/dev/log')
+
+        # set the logging level for the log handler(s)
+        sh.setLevel(logging.INFO)
+        slh.setLevel(logging.INFO)
+
+        # set the message format for the logger
+        # formatter = logging.Formatter('%(module)s.%(funcName)s: %(message)s')
+        formatter = logging.Formatter('%(name)-12s: %(levelname)-8s: %(message)s')
+        sh.setFormatter(formatter)
+        slh.setFormatter(formatter)
+
+        # add the handler to the logger
+        log = logging.getLogger(__name__)
+        log.addHandler(sh)
+        log.addHandler(slh)
+
