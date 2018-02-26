7a0068be02b9d8b9f87e1ee186f6bf20a9557c3f
erenisr <erenisramadani@gmail.com>
2018-02-26 20:26:09 +0100

    Added: Method to handle profile rename, both, as separated method and within update

--- api/src/models/LXCProfile.py
+++ api/src/models/LXCProfile.py
@@ -15,6 +15,12 @@ class LXCProfile(LXDModule):
         except Exception as e:
             raise ValueError(e)
 
+    def info(self, name):
+        try:
+            return self.client.api.profiles[name].get().json()['metadata']
+        except Exception as e:
+            raise ValueError(e)
+
     def createProfile(self):
         try:
             self.client.profiles.create(self.input.get('name'), config=self.input.get('config'),
@@ -32,6 +38,16 @@ class LXCProfile(LXDModule):
     def updateProfile(self):
         try:
             self.client.api.profiles[self.input.get('name')].put(json=self.input).json()['metadata']
+            if self.input.get('new_name'):
+                return self.rename()
             return self.info()
+        except Exception as e:
+            raise ValueError(e)
+
+    def rename(self):
+        try:
+            profile = self.client.profiles.get(self.input.get('name'))
+            profile.rename(self.input.get('new_name'))
+            return self.info(self.input.get('new_name'))
         except Exception as e:
             raise ValueError(e)
