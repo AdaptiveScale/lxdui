a994081c239b819b82ac4d96ee4dae6c23ab6785
vetoni <thajdari@cask.co>
2018-02-26 09:59:36 -0600

    Revert: Refactored CLI implementation

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -9,10 +9,19 @@ log = logging.getLogger(__name__)
 l = logger.Log(__name__)
 # l.file = '/Users/vetoni/PycharmProjects/lxdui/conf/lxdui.log'
 
-c = conf.Config(conf='/Users/vetoni/PycharmProjects/lxdui/conf/lxdui.conf')
+# c = conf.Config(conf='/Users/vetoni/PycharmProjects/lxdui/conf/lxdui.conf')
 # c.show()
-c.setEnv()
-c.showEnv()
-k, i = c.getEnv()
-print(k)
-print(i)
+# c.setEnv()
+# c.showEnv()
+# k, i = c.getEnv()
+# print(k)
+# print(i)
+
+# env = ['LXDUI_LOG_DIR', 'LXDUI_LOG_FILE', 'LXDUI_CONF_DIR', 'LXDUI_CONF_FILE']
+# kv = os.environ.keys()
+# for k in kv:
+#     print(k)
+
+env = ['PATH']
+for ev in env:
+    print(ev, ' = ', os.environ.get(ev))
