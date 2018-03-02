4cf604b7101925be073a69e583a675e54be882bf
nuhi <nuhi@adaptivescale.com>
2018-03-02 11:18:12 +0100

    Get lxdbr0. Update lxdb0.

--- api/src/core.py
+++ api/src/core.py
@@ -14,6 +14,9 @@ app.register_blueprint(image_api, url_prefix='/api/image')
 from api.src.controllers.profile import profile_api
 app.register_blueprint(profile_api, url_prefix='/api/profile')
 
+from api.src.controllers.network import network_api
+app.register_blueprint(network_api, url_prefix='/api/network')
+
 @app.cli.command
 def run():
     startApp()
