23511c9309520adcc34b8a6cea60c7bcaeabe370
Ajdin Idrizi <ajdin.idrizi@gmail.com>
2018-03-05 13:30:49 +0100

    Revert "Rebase with the refactored CLI implementation"

--- cli/cli.py
+++ cli/cli.py
@@ -43,10 +43,6 @@ class Init(object):
     - Generate the client certificates is they don't exist and prompt the user to
       overwrite if they do exit
 
-    If the config file does not exit or has not been provided by the user,
-     then create
-
-
     """
 
     def __init__(self, password):
@@ -101,7 +97,6 @@ class Init(object):
 
         if file_type == 'cert':
             Certificate.save(self.cert)
-
 
 class User(object):
     def __init__(self):
