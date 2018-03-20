bf5f420a0e39de7a08779b25f816d557728ed8fd
ajdin <ajdin.idrizi@gmail.com>
2018-03-20 16:24:48 +0100

    added: images

--- app/ui/blueprint.py
+++ app/ui/blueprint.py
@@ -9,31 +9,30 @@ def index():
     return render_template('login.html', currentpage='Login')
 
 @uiPages.route('/containers')
-# @login_required
 def container():
     containers = LXDModule().listContainers()
     return render_template('containers.html', currentpage='Containers',
                            containers=containers, lxdui_current_version='2.0')
 
 @uiPages.route('/profiles')
-# @login_required
 def profile():
     profiles = LXDModule().listProfiles()
     return render_template('profiles.html', currentpage='Profiles',
                            profiles=profiles, lxdui_current_version='2.0')
 @uiPages.route('/network')
-# @login_required
 def network():
     network = LXDModule().listNetworks()
     return render_template('network.html', currentpage='Network',
                            network=network, lxdui_current_version='2.0')
 
 @uiPages.route('/images')
-# @login_required
 def images():
     localImages = LXDModule().listLocalImages()
-    remoteImages = LXDModule().listRemoteImages()
+    try:
+        remoteImages = LXDModule().listRemoteImages()
+    except:
+        remoteImages = []
+        # TODO - log exception
     return render_template('images.html', currentpage='Images',
                            localImages=localImages,
                            remoteImages=remoteImages, lxdui_current_version='2.0')
-
