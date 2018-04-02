a98ca618f87ea179f3121389eb2b5b5bd272e92a
nuhi <nuhi@adaptivescale.com>
2018-04-02 16:34:01 +0200

    Fix LXD not Installed

--- app/ui/blueprint.py
+++ app/ui/blueprint.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect
 from app.api.models.LXCContainer import LXDModule, LXCContainer
+from app.__metadata__ import VERSION
 import json
 
 uiPages = Blueprint('uiPages', __name__, template_folder='./templates',
@@ -11,45 +12,69 @@ def index():
 
 @uiPages.route('/containers')
 def container():
-    containers = LXDModule().listContainers()
-    result = []
-    for container in containers:
-        c = LXCContainer({'name': container.get('name')})
-        result.append(c.info())
-    images = LXDModule().listLocalImages()
-    profiles = LXDModule().listProfiles()
-    return render_template('containers.html', currentpage='Containers',
-                           containers=result,
-                           images = images,
-                           profiles = profiles,
-                           lxdui_current_version='2.0')
+    try:
+        containers = LXDModule().listContainers()
+        result = []
+        for container in containers:
+            c = LXCContainer({'name': container.get('name')})
+            result.append(c.info())
+        images = LXDModule().listLocalImages()
+        profiles = LXDModule().listProfiles()
+        return render_template('containers.html', currentpage='Containers',
+                               containers=result,
+                               images = images,
+                               profiles = profiles,
+                               lxdui_current_version=VERSION)
+    except:
+        return render_template('containers.html', currentpage='Containers',
+                               containers=[],
+                               images=[],
+                               profiles=[],
+                               lxdui_current_version=VERSION)
 
 @uiPages.route('/profiles')
 def profile():
-    profiles = LXDModule().listProfiles()
-    return render_template('profiles.html', currentpage='Profiles',
-                           profiles=profiles, lxdui_current_version='2.0')
+    try:
+        profiles = LXDModule().listProfiles()
+        return render_template('profiles.html', currentpage='Profiles',
+                               profiles=profiles, lxdui_current_version=VERSION)
+    except:
+        return render_template('profiles.html', currentpage='Profiles',
+                               profiles=[], lxdui_current_version=VERSION)
+
 @uiPages.route('/network')
 def network():
-    network = LXDModule().listNetworks()
-    return render_template('network.html', currentpage='Network',
-                           network=network, lxdui_current_version='2.0')
+    try:
+        network = LXDModule().listNetworks()
+        return render_template('network.html', currentpage='Network',
+                               network=network, lxdui_current_version=VERSION)
+    except:
+        return render_template('network.html', currentpage='Network',
+                               network=[], lxdui_current_version=VERSION)
 
 @uiPages.route('/images')
 def images():
-    localImages = LXDModule().listLocalImages()
-    profiles = LXDModule().listProfiles()
     try:
+        localImages = LXDModule().listLocalImages()
+        profiles = LXDModule().listProfiles()
         remoteImages = LXDModule().listRemoteImages()
+        return render_template('images.html', currentpage='Images',
+                               localImages=localImages,
+                               remoteImages=remoteImages,
+                               profiles=profiles,
+                               jsData={
+                                   'local': json.dumps(localImages),
+                                   'remote': json.dumps(remoteImages),
+                               },
+                               lxdui_current_version=VERSION)
     except:
-        remoteImages = []
         # TODO - log exception
-    return render_template('images.html', currentpage='Images',
-                           localImages=localImages,
-                           remoteImages=remoteImages,
-                           profiles=profiles,
-                           jsData = {
-                               'local': json.dumps(localImages),
-                               'remote':json.dumps(remoteImages),
-                           },
-                           lxdui_current_version='2.0')
+        return render_template('images.html', currentpage='Images',
+                               localImages=[],
+                               remoteImages=[],
+                               profiles=[],
+                               jsData={
+                                   'local': json.dumps([]),
+                                   'remote': json.dumps([]),
+                               },
+                               lxdui_current_version=VERSION)
