e70a5f51f60c946b6550d5f66ac0695de928d338
nuhi <nuhi@adaptivescale.com>
2018-04-13 14:01:01 +0200

    Implement Snapshot Functionalities

--- app/api/controllers/snapshot.py
+++ app/api/controllers/snapshot.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from flask_jwt import jwt_required
+from app.api.schemas.container_schema import doValidateCloneMove
 
 from app.api.models.LXCSnapshot import LXCSnapshot
 from app.api.utils import response
@@ -38,6 +39,42 @@ def createSnapshot(name, container):
         return response.replyFailed(message=e.__str__())
 
 
+@snapshot_api.route('/<string:name>/container/<string:container>', methods=['PUT'])
+@jwt_required()
+def restoreSnapshot(name, container):
+    try:
+        snapshot = LXCSnapshot({'name': name, 'container': container})
+        return response.replySuccess(snapshot.snapshotRestore())
+    except ValueError as e:
+        return response.replyFailed(message=e.__str__())
+
+
+@snapshot_api.route('/<string:name>/container/<string:container>/publish', methods=['POST'])
+@jwt_required()
+def publishSnapshot(name, container):
+    try:
+        snapshot = LXCSnapshot({'name': name, 'container': container})
+        return response.replySuccess(snapshot.snapshotPublish())
+    except ValueError as e:
+        return response.replyFailed(message=e.__str__())
+
+@snapshot_api.route('/<string:name>/container/<string:container>/create', methods=['POST'])
+@jwt_required()
+def createContainerSnapshot(name, container):
+    input = request.get_json(silent=True)
+    validation = doValidateCloneMove(input)
+    if validation:
+        return response.reply(message=validation.message, status=403)
+
+    input['name'] = name
+    input['container'] = container
+    try:
+        snapshot = LXCSnapshot(input)
+        return response.replySuccess(snapshot.snapshotCreateContainer())
+    except ValueError as e:
+        return response.replyFailed(message=e.__str__())
+
+
 @snapshot_api.route('/<string:name>/container/<string:container>', methods=['DELETE'])
 @jwt_required()
 def deleteSnapshot(name, container):
@@ -46,3 +83,5 @@ def deleteSnapshot(name, container):
         return response.replySuccess(snapshot.snapshotDelete())
     except ValueError as e:
         return response.replyFailed(message=e.__str__())
+
+
