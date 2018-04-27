b31506198e9cb0b6239e8a5eb8eb7ce9bd71e358
ajdin <ajdin.idrizi@gmail.com>
2018-04-27 13:32:57 +0200

    fixed: Token will be renewed from UI while the user is active

--- app/ui/static/js/app.js
+++ app/ui/static/js/app.js
@@ -13,6 +13,7 @@ var App = App || {
     network: null,
     location: null,
     loading: false,
+    tokenRefreshing: false,
     notInitialized:['containers', 'images', 'network', 'containerDetails'],
     init: function(){
         console.log('App initializing');
@@ -52,19 +53,17 @@ var App = App || {
         $('#stamplike').text('LXD is not Installed').removeClass('label-success').addClass('label-danger');
     },
     setDefaultHeaders: function(){
-        console.log('locaiton', window.location.href, WEB);
-        if(!localStorage.getItem('authToken') && window.location.href!==WEB){
+        if(!sessionStorage.getItem('authToken') && window.location.href!==WEB){
             console.log('Not Logged In/Token Expired - Redirecting to login');
             return window.location = WEB;
         }
         if(window.location.href!==WEB){
-            console.log('Setting Authorization header', localStorage.getItem('authToken'));
             $.ajaxSetup({
                 headers:{
-                    Authorization:'JWT '+localStorage.getItem('authToken'),
+                    Authorization:'JWT '+sessionStorage.getItem('authToken'),
                     'Content-Type':'application/json'
                 },
-                beforeSend: function() {
+                beforeSend: function(xhr, settings) {
                     App.ongoingOperation +=1;
                     $('.loader').show();
                 },
@@ -73,20 +72,46 @@ var App = App || {
                     if(response.status == 401 && window.location!== WEB){
                         window.location = WEB;
                     }
-                    if(window.location!== WEB){
-                        App.triggerAlert(response, this.type    , this.url);
+                    if((App.helpers.parseJwt(sessionStorage.getItem('authToken')).exp-App.helpers.currentAppTime())<120){
+                        if(!App.tokenRefreshing){
+                            App.updateTokenExpiration.call(App);
+                        }
                     }
                     if(App.ongoingOperation ==0)
                         $('.loader').hide();
+                    if(App.tokenRefreshing){
+                        return;
+                    }
+                    if(window.location!== WEB){
+                        App.triggerAlert(response, this.type, this.url);
+                    }
                 }
             });
         }
+    },
+    updateTokenExpiration: function(){
+        App.tokenRefreshing = true;
+        $.ajax({
+            url:App.baseAPI+'user/login',
+            method:'POST',
+            contentType: "application/json; charset=utf-8",
+            dataType:'json',
+            data:sessionStorage.getItem('user'),
+            success: $.proxy(this.tokenUpdateSuccess, this)
+        });
+    },
+    tokenUpdateSuccess:function(response){
+        sessionStorage.setItem('authToken', response.access_token);
+        App.tokenRefreshing=false;
     },
     setActiveLink: function(name) {
         $('.nav li').removeClass('active');
         $('.'+name+'-menu').addClass('active');
     },
     triggerAlert: function(response, method, url){
+        if(url.endsWith('login')){
+            return;
+        }
         if([undefined, 'GET'].indexOf(method)>-1)
             return;
         var tempResp = response.responseJSON || response;
@@ -111,6 +136,14 @@ var App = App || {
     helpers:{
         capitalizeFirstLetter: function(value){
             return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        currentAppTime: function(){
+            return Math.floor(new Date().getTime()+new Date().getTimezoneOffset())/1000;
+        },
+        parseJwt: function(token) {
+            var base64Url = token.split('.')[1];
+            var base64 = base64Url.replace('-', '+').replace('_', '/');
+            return JSON.parse(window.atob(base64));
         }
     }
 };
