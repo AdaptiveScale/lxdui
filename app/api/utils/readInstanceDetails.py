5218c049670b560ea49afd803c2f3f0b10e01fdd
nuhi <nuhi@adaptivescale.com>
2018-05-04 11:43:59 +0200

    Fix Bug If LXD Module is not installed. Fix width for modal in image details

--- app/api/utils/readInstanceDetails.py
+++ app/api/utils/readInstanceDetails.py
@@ -6,14 +6,26 @@ import logging
 def readInstanceDetails():
     instanceDetails = ("Python Version: {}".format(platform.python_version()))
     instanceDetails +=("\nPython Path: {}".format(' '.join(path for path in sys.path)))
-    instanceDetails +=("\nLXD Version: {}".format(LXDModule().config()['environment']['server_version']))
-    instanceDetails +=("\nLXD Status: {}".format(LXDModule().config()['api_status']))
+    instanceDetails +=("\nLXD Version: {}".format(getLXDInfo()['environment']['server_version']))
+    instanceDetails +=("\nLXD Status: {}".format(getLXDInfo()['api_status']))
     instanceDetails +=("\nOS: {}".format(platform.platform()))
     instanceDetails +=("\nLXDUI Path: {}".format(sys.path[0]))
     instanceDetails +=("\nCPU: {}".format(getProcessorDetails()))
     instanceDetails +=("\nMemory: {}MB".format(getMemory()))
     instanceDetails +=("\nDisk: {}".format(getDiskDetails()))
     logging.info(instanceDetails)
+
+def getLXDInfo():
+    try:
+        info = LXDModule().config()
+        return info
+    except:
+        return {
+            'environment': {
+                'server_version': 'N/A'
+            },
+            'api_status': 'N/A'
+        }
 
 def getMemory():
     mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
