1f61ece6f6b0950706ebef159665eafbfeeaa1fd
nuhi <nuhi@adaptivescale.com>
2018-05-05 20:07:45 +0200

    Fix container list bug when missing image

--- app/api/utils/containerMapper.py
+++ app/api/utils/containerMapper.py
@@ -6,11 +6,15 @@ def getContainerDetails(container):
         if len(container.state().network.get('eth0')['addresses']) > 0:
             ip = container.state().network['eth0']['addresses'][0].get('address', 'N/A')
 
+    image = 'N/A'
+    if container.config.get('image.os') != None and container.config.get('image.release') != None and container.config.get('image.architecture') != None:
+        image = ''.join(container.config.get('image.os') + ' ' + container.config.get('image.release') + ' ' + container.config.get('image.architecture'))
+
     return {
         'name': container.name,
         'status': container.status,
         'ip': ip,
         'ephemeral': container.ephemeral,
-        'image': ''.join(container.config.get('image.os') + ' ' + container.config.get('image.release') + ' ' + container.config.get('image.architecture')),
+        'image': image,
         'created_at': container.created_at
     }
