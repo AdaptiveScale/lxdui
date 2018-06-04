ee48c4ef22804fc72f1cfade9f7078b22e74ec7c
nuhi <nuhi@adaptivescale.com>
2018-06-04 12:02:51 +0200

    Create Multiple Containers Advanced Settings

--- app/ui/static/js/images.js
+++ app/ui/static/js/images.js
@@ -58,9 +58,38 @@ App.images = App.images || {
         $('#selectAllRemote').on('change', $.proxy(this.toggleSelectAll, this, 'Remote'));
         this.itemTemplate = $('.itemTemplate').clone();
         $('#modalDownloadButton').on('click', $.proxy(this.doDownload, this));
+
+        this.initKeyValuePairs();
     },
     convertImageSize:function(index, item){
         $(item).text(App.formatBytes($(item).text()));
+    },
+    initKeyValuePairs: function() {
+        i=0;
+        for (key in App.properties.keyValues) {
+            $('#advancedSettingsMultipleContainer'+i).append('<div class="row">' +
+                    '<div class="col-lg-5">'+
+                        '<div class="form-group row">' +
+                            '<input type="text" class="form-control" placeholder="' + key + '"  disabled />' +
+                            '<a href="#" class="hover-info" onmouseover="$.proxy(App.containerDetails.showPopover(this));" title="Information" data-toggle="popover" data-trigger="hover" data-content="'+ App.properties.keyValues[key].description + '" data-original-title="Information">' +
+                                 '<span class="glyphicon glyphicon-info-sign"></span>' +
+                             '</a>' +
+                        '</div>' +
+                    '</div>'+
+                    '<div class="col-lg-5">' +
+                        '<div class="form-group row">' +
+                            '<input type="text" name="'+ key +'" id="' + key + '" class="form-control" placeholder="" value="" disabled />' +
+                        '</div>' +
+                    '</div>' +
+                     '<div class="col-lg-2">' +
+                         '<div class="btn-group" role="group">' +
+                            '<button type="button" id="" class="formModifier btn btn-sm btn-default" onClick="$.proxy(App.containerDetails.enableInput(this));">On</button>' +
+                            '<button type="button" id="" class="formModifier btn btn-sm btn-danger" onClick="$.proxy(App.containerDetails.disableInput(this));">Off</button>' +
+                        '</div>' +
+                     '</div>' +
+                '</div>');
+            i+=1;
+        }
     },
     setLoading: function(state){
         var tempLoaderState = state?'show':'hide';
