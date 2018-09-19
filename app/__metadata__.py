dac0387ac8bf0f6651611f7668979c51fe794652
Felix Engelmann <fe-github@nlogn.org>
2018-09-20 00:59:38 +0200

    main functionality with remote lxd socket, terminal not working

--- app/__metadata__.py
+++ app/__metadata__.py
@@ -23,6 +23,8 @@ __default_config__ = """
 [LXDUI]
 lxdui.port = 15151
 lxdui.images.remote = https://images.linuxcontainers.org
+#lxdui.lxd.remote = https://lxd.host.org:8443/
+#lxdui.lxd.sslverify = true
 lxdui.jwt.token.expiration = 1200
 lxdui.jwt.secret.key = AC8d83&21Almnis710sds
 lxdui.jwt.auth.url.rule = /api/user/login
