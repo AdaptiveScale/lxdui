0a52998133527a333bbb75030c3b55d1e672697a
Ye Yang <ye.yang98@hotmail.com>
2022-01-24 12:18:23 +0000

    Extract additional instance information

--- app/api/utils/containerMapper.py
+++ app/api/utils/containerMapper.py
@@ -1,21 +1,20 @@
 def getContainerDetails(container):
-    ip = 'N/A'
-    if container.state().network != None:
-        if  container.state().network.get('eth0') != None and len(container.state().network.get('eth0')['addresses']) > 0:
-            ip = container.state().network['eth0']['addresses'][0].get('address', 'N/A')
-        elif container.state().network.get('enp5s0') != None and len(container.state().network.get('enp5s0')['addresses']) > 0:
-            ip = container.state().network['enp5s0']['addresses'][0].get('address', 'N/A')
-        elif ip == 'N/A':
-            found = False
-            res = container.state().network
-            for network in res:
-                for address in res[network]["addresses"]:
-                    if address['family'] == 'inet' and address['scope'] == 'global':
-                        ip = address['address']
-                        found = True
-                        break
-                if found:
-                    break
+    ip = []
+
+    # Retrieve all IPv4 addresses with global scope
+    instanceNetwork = container.state().network
+    if instanceNetwork != None:
+        for network_interface in instanceNetwork:
+            current_network = instanceNetwork.get(network_interface)
+            network_addresses = current_network.get('addresses')
+            if len(network_addresses) > 0:
+                for address in network_addresses:
+                    if isIPV4(address.get('address')) and (address.get('scope') == 'global'):
+                        ip.append(address.get('address'))
+
+    # in case no IP addresses were found in all the previous network interfaces
+    if len(ip) == 0:
+        ip = 'N/A'
 
     image = 'N/A'
     if container.config.get('image.os') != None and container.config.get('image.release') != None and container.config.get('image.architecture') != None:
@@ -24,12 +23,31 @@ def getContainerDetails(container):
     instance_type = 'Container'
     if container.type == 'virtual-machine':
         instance_type = 'Virtual Machine'
+
+    memory = container.config.get('limits.memory')
+    if memory is None:
+        memory = 'N/A'
+
+    cpu = container.config.get('limits.cpu')
+    if cpu is None:
+        cpu = 'N/A'
+
+    try:
+        # attempt to extract the size of the root disk
+        disk = container.devices.get('root')['size']
+    except:
+        # in case the root device was not set (passed on through profile)
+        disk = 'N/A'
+
     return {
         'name': container.name,
         'status': container.status,
         'ip': ip,
+        'memory': memory,
+        'cpu': cpu,
+        'disk': disk,
         'ephemeral': container.ephemeral,
         'type': instance_type,
         'image': image,
-        'created_at': container.created_at
+        'created_at': container.created_at,
     }
